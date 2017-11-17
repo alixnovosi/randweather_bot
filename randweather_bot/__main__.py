cff160ad853cdd01d9042e090ff984f556381258
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-11-17 13:13:06 -0800

    use weatherbotskeleton to avoid duplicating work

--- randweather_bot/__main__.py
+++ randweather_bot/__main__.py
@@ -4,10 +4,7 @@ import random
 import time
 from os import path
 
-import botskeleton
-import tweepy
-
-import weather_gen
+import weatherbotskeleton
 
 # Delay between tweets in seconds.
 DELAY = 1800
@@ -15,22 +12,20 @@ DELAY_VARIATION = 600
 
 if __name__ == "__main__":
     SECRETS_DIR = path.join(path.abspath(path.dirname(__file__)), "SECRETS")
-    BOT_SKELETON = botskeleton.BotSkeleton(SECRETS_DIR, bot_name="randweather_bot")
+    BOT_SKELETON = weatherbotskeleton.WeatherbotSkeleton(
+        SECRETS_DIR,
+        owner_url="https://github.com/andrewmichaud/randweather_bot",
+        bot_name="randweather_bot")
 
-    LOG = botskeleton.set_up_logging()
+    LOG = weatherbotskeleton.set_up_logging()
 
     while True:
         LOG.info("Sending a weather tweet.")
-        try:
-            weather = weather_gen.produce_status()
-        except Exception as e:
-            BOT_SKELETON.send_dm_sos(f"Bot {BOT_SKELETON.bot_name} " +\
-                                     f"had an error it can't recover from!\n{e}")
-            raise
+        weather = BOT_SKELETON.produce_status()
 
         LOG.info(f"Sending:\n {weather}")
-
         BOT_SKELETON.send(weather)
 
-        BOT_SKELETON.delay = random.choice(range(DELAY-DELAY_VARIATION, DELAY+DELAY_VARIATION+1))
+        BOT_SKELETON.set_delay(random.choice(range(DELAY-DELAY_VARIATION,
+                                                   DELAY+DELAY_VARIATION+1)))
         BOT_SKELETON.nap()
