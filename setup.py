cb4f39219477a946443d074da2a2d3e3e610a32f
Andrew Michaud <dev@drew.life>
2019-03-08 00:32:51 -0800

    patched weatherbotskeleton

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["randomweather_bot = randomweather_bot.__main__:main"]
       },
 
-      install_requires=["weatherbotskeleton>=1.4.2"],
+      install_requires=["weatherbotskeleton>=1.4.3"],
 
       license="BSD3",
 
