764866ce58260aa73e003d61447346822449a714
Andrew Michaud <dev@drew.life>
2019-05-30 14:43:22 -0700

    new skeleton

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["randomweather_bot = randomweather_bot.__main__:main"]
       },
 
-      install_requires=["weatherbotskeleton>=1.5.0"],
+      install_requires=["weatherbotskeleton>=1.5.1"],
 
       license="BSD3",
 
