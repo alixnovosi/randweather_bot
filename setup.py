5b60c10aa0035339953680946ead002c30a039d4
Andrew Michaud <dev@drew.life>
2019-05-30 15:22:54 -0700

    repin...

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["randomweather_bot = randomweather_bot.__main__:main"]
       },
 
-      install_requires=["weatherbotskeleton>=1.5.1"],
+      install_requires=["weatherbotskeleton>=1.5.3"],
 
       license="BSD3",
 
